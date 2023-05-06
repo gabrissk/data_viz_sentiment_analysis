4ba202742dc939aa8d87f0f12997897f352c5038
gabriel.morais <gabriel.morais@dtidigital.com.br>
2023-05-05 23:21:41 -0300

    Reading configs from file., added logging

--- src/reddit_scraper.py
+++ src/reddit_scraper.py
@@ -1,9 +1,18 @@
+import logging
 import praw
 from kafka import KafkaProducer
 import json
+import yaml
+
+import os
+config_file_path = os.path.abspath("../config/scraper_config.yaml")
+
+
+with open('config/scraper_config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
 def process_comment(comment, new_post):
-    print(comment.body)
+    #print(comment.body)
     new_post['comments'].append(
         {
             "id": comment.id, 
@@ -16,9 +25,9 @@ def process_comment(comment, new_post):
         process_comment(reply, new_post)
 
 
-def process_subreddit_client_comments(reddit_client:praw.Reddit, subject:str, posts:list):
+def process_subreddit_client_comments(reddit_client:praw.Reddit, subject:str, posts:list, posts_limit:int=10):
     subreddit_client = reddit_client.subreddit(subject)
-    for post in subreddit_client.new(limit=10):
+    for post in subreddit_client.new(limit=posts_limit):
         # print(post.title)
         # print(post.selftext)
         # print(post.created_utc)
@@ -38,27 +47,30 @@ def process_subreddit_client_comments(reddit_client:praw.Reddit, subject:str, po
 
 
 def send_post_to_kafka(posts:list):
-    print("Sending to kafka...")
-    with KafkaProducer(bootstrap_servers='localhost:9092') as producer:
+    logging.info('Sending messages to kafka...')
+    with KafkaProducer(bootstrap_servers=config['kafka']['server']) as producer:
         # post_dict = vars(posts)
-        producer.send("reddit_client-dataviz-comments", json.dumps(posts).encode('utf-8')).get(timeout=10)
+        producer.send(config['kafka']['topic_name'], json.dumps(posts).encode('utf-8')).get(timeout=10)
         producer.flush()
 
 
 def main():
+    logging.info('Starting job...')
     # 'reddit' object from the praw  reddit_client API
-    reddit_client = praw.Reddit(client_id='yQ83iAt0TWpfgiW07sTkOw', client_secret='q9na4iAgw25R8yBgLIDiNVaMwSp7EQ', user_agent='gabrissk', check_for_async=False)
+    reddit_client = praw.Reddit(client_id=config['reddit_api']['client_id'], client_secret=config['reddit_api']['client_secret'], 
+                                user_agent=config['reddit_api']['user_agent'], check_for_async=False)
     # subreddit_clients to search
-    subjects = ["powerbi", "tableau", "qlikview", "looker", "datastudio", "domo"]
+    subjects = ['powerbi', 'tableau', 'qlikview', 'looker', 'datastudio', 'domo']
 
     posts = []
 
     for subject in subjects:
-        process_subreddit_client_comments(reddit_client, subject, posts)
-        print("Finished reading from ", subject)
+        process_subreddit_client_comments(reddit_client, subject, posts, 10)
+        logging.info('Finished reading from ' + subject)
 
     send_post_to_kafka(posts)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     main()
